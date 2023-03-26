56c850ca6bd6b6d61891b27284ba0ac9cfaaa3d4
Michael Arcidiacono <mixarcidiacono@gmail.com>
2023-03-26 09:43:11 -0400

    batch methods for graphbatches as well

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name="terrace",
-    version="0.0.74",
+    version="0.0.75",
     author="Michael Brocidiacono",
     author_email="",
     description="high level PyTorch utils",
