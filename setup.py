976998cd8adbefb9cf146f06a86fef32cd252e30
Michael Arcidiacono <mixarcidiacono@gmail.com>
2023-04-26 09:49:00 -0400

    fixed bug in lazybatch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name="terrace",
-    version="0.0.76",
+    version="0.0.77",
     author="Michael Brocidiacono",
     author_email="",
     description="high level PyTorch utils",
