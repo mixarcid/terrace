a73f87ec11bcc7ecef2991a259387ae78f0feb7d
Michael Arcidiacono <mixarcidiacono@gmail.com>
2023-05-11 16:47:56 -0400

    Terrace will now still work even if you don't have DGL

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name="terrace",
-    version="0.0.82",
+    version="0.0.90",
     author="Michael Brocidiacono",
     author_email="",
     description="high level PyTorch utils",
--- src/terrace/__init__.py
+++ src/terrace/__init__.py
@@ -1,5 +1,12 @@
 from .module import *
 from .categorical_tensor import CategoricalTensor, NoStackCatTensor
-from .graph import Graph, GraphBatch
+
+# if DGL doesn't exist, the Graph class won't work.
+# that's fine if the user doesn't want to use that feature
+try:
+    from .graph import Graph, GraphBatch
+except ModuleNotFoundError:
+    pass
+
 from .batch import Batchable, BatchBase, Batch, LazyBatch, collate, DataLoader, NoStackTensor
 from .dataframe import DFRow
