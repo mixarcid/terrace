86baede34bd67e9e75fc18773621fdaa8cd782db
Michael Arcidiacono <mixarcidiacono@gmail.com>
2023-10-18 18:14:35 -0400

    inc version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name="terrace",
-    version="0.0.90",
+    version="0.0.91",
     author="Michael Brocidiacono",
     author_email="",
     description="high level PyTorch utils",
